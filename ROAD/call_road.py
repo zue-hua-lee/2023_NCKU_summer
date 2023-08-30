8a888b25f3ff775c0c55c3857b33bc55bd26343c
s3839083 <e24094237@gs.ncku.edu.tw>
2023-08-30 14:45:40 +0800

    road.4

--- ROAD/call_road.py
+++ ROAD/call_road.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
 
     args = sys.argv
     
+    if (len(sys.argv) != 3):
+        print('error')
+
     script_name = args[0]
     nowtime = int(args[1])
     ev_new = int(args[2])
